dd635fcbef1dadb52fe02033d1034ea6cd8e8c55
Ceyda <ceydaonemli2@gmail.com>
2022-10-04 01:20:03 +0300

    card and input component created

--- kanban-board/src/components/List/List.js
+++ kanban-board/src/components/List/List.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Paper, Typography, CssBaseline } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import Title from "./Title"
+import Card from "../Card"
+import InputContainer from "../Input/InputContainer"
 const useStyle = makeStyles((theme) => ({
   root: {
     width: "300px",
     backgroundColor: "#EBECF0",
-    marginLeft:theme.spacing(1),
+    marginLeft: theme.spacing(1),
   },
 }))
 const List = () => {
@@ -15,7 +17,9 @@ const List = () => {
     <div>
       <Paper className={classes.root}>
         <CssBaseline />
-      <Title/>
+        <Title />
+        <Card />
+        <InputContainer />
       </Paper>
     </div>
   )
--- kanban-board/src/components/List/Title.js
+++ kanban-board/src/components/List/Title.js
@@ -11,8 +11,12 @@ const useStyle = makeStyles((theme) => ({
   editableTitle: {
     marginLeft: theme.spacing(1),
     flexGrow: 1,
+    fontSize: "1.2rem",
+    fontWeight: "bold",
   },
   input: {
+    fontSize: "1.2rem",
+    fontWeight: "bold",
     margin: theme.spacing(1),
     "&:focus": {
       background: "#ddd",
@@ -28,6 +32,7 @@ const Title = () => {
       {open ? (
         <div>
           <InputBase
+            autoFocus
             value="Todo"
             inputProps={{
               className: classes.input,
